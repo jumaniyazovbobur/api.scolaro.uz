6bb96180e40e58065fa484948b35f8f1525e3a30
Bekzod Nosirov <bekzodnosirov02@gmail.com>
2023-09-22 14:16:29 +0500

    user and consulting service added

--- src/main/java/api/scolaro/uz/controller/profile/UserController.java
+++ src/main/java/api/scolaro/uz/controller/profile/UserController.java
@@ -1,8 +1,11 @@
 package api.scolaro.uz.controller.profile;
 
+import api.scolaro.uz.dto.profile.UserUpdateDTO;
+import api.scolaro.uz.service.profile.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,30 +14,40 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @Tag(name = "User api list", description = "Api list for user")
 public class UserController {
-    @PutMapping("/{userId}")
+
+    @Autowired
+    private UserService userService;
+
+    @PutMapping("/{id}")
     @Operation(summary = "Update api", description = "")
-    public ResponseEntity<?> update(@PathVariable("userId") String userId){
-        return null;
+    public ResponseEntity<?> update(@PathVariable("id") String id,
+                                    @RequestBody UserUpdateDTO updateDTO) {
+        log.info("Update user {}", updateDTO);
+        return ResponseEntity.ok(userService.update(id,updateDTO));
     }
-    @GetMapping("/{userId}")
+
+    @GetMapping("/{id}")
     @Operation(summary = "Get by id api", description = "")
-    public ResponseEntity<?> getId(){
+    public ResponseEntity<?> getId(@PathVariable("id") String id) {
+        log.info("Get user {}", id);
         return null;
     }
+
     @GetMapping("/all")
     @Operation(summary = "Get all api", description = "")
-    public ResponseEntity<?> getAll(){
+    public ResponseEntity<?> getAll() {
         return null;
     }
 
     @GetMapping("/filter")
     @Operation(summary = "Filter user api", description = "")
-    public ResponseEntity<?> filter(){
+    public ResponseEntity<?> filter() {
         return null;
     }
-    @GetMapping("/{userId}")
+
+    @GetMapping("/{id}")
     @Operation(summary = "Deleted user api", description = "")
-    public ResponseEntity<?> deleted(){
+    public ResponseEntity<?> deleted(@PathVariable("id") String id) {
         return null;
     }
 
--- src/main/java/api/scolaro/uz/service/profile/UserService.java
+++ src/main/java/api/scolaro/uz/service/profile/UserService.java
@@ -1,7 +1,11 @@
 package api.scolaro.uz.service.profile;
 
+import api.scolaro.uz.dto.profile.UserUpdateDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    public Object update(String id, UserUpdateDTO updateDTO) {
+        return null;
+    }
 }
