8b4e523f4dad1d128cb25fd944d19afe58bba331
Bekzod Nosirov <bekzodnosirov02@gmail.com>
2023-09-23 12:10:35 +0500

    fix

--- src/main/java/api/scolaro/uz/repository/profile/ConsultingRepository.java
+++ src/main/java/api/scolaro/uz/repository/profile/ConsultingRepository.java
@@ -2,9 +2,19 @@ package api.scolaro.uz.repository.profile;
 
 import api.scolaro.uz.entity.profile.ConsultingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
-public interface ConsultingRepository extends JpaRepository<String, ConsultingEntity> {
+public interface ConsultingRepository extends JpaRepository<ConsultingEntity, String> {
 
+    Optional<ConsultingEntity> findByIdAndVisibleTrue(String id);
+    @Transactional
+    @Modifying
+    @Query("update ConsultingEntity set visible=true , deletedDate=now() where id=:id")
+    void deleted(String id);
 }
