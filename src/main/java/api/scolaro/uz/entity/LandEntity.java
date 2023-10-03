ea203f5c32735350ef3aaf1ff26418b41289e5cf
Bekzod Nosirov <bekzodnosirov02@gmail.com>
2023-10-03 20:56:20 +0500

    fix

---
+++ src/main/java/api/scolaro/uz/entity/LandEntity.java
@@ -0,0 +1,19 @@
+package api.scolaro.uz.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "land")
+public class LandEntity extends BaseIdentityEntity{
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "order_by")
+    private Integer orderBy;
+}
