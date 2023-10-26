d40d4080a85a48c3872af52c960070be76db9681
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2023-10-26 14:06:54 +0500

    ConsultingComment bug fixed

--- src/main/java/api/scolaro/uz/service/AppApplicationService.java
+++ src/main/java/api/scolaro/uz/service/AppApplicationService.java
@@ -150,11 +150,17 @@ public class AppApplicationService {
         return dto;
     }
 
+    public AppApplicationEntity getByStudentIdAndConsultingId(String studentId, String consultingId){
+        Optional<AppApplicationEntity> appApplication = appApplicationRepository.findByStudentIdAndConsultingIdAndVisibleTrue(studentId, consultingId);
+        return appApplication.orElse(null);
+    }
     public AppApplicationEntity getByIdForSimpleMessage(String id){
+
         return appApplicationRepository.findByIdAndVisibleTrue(id).orElseThrow(() -> {
             log.warn("Application not Found");
             throw new ItemNotFoundException("Application not found");
         });
     }
+
 
 }
