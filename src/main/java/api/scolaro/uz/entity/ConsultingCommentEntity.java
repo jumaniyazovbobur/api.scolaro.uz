34e5a047487f697073cbb98bd50a713cd8505d1a
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2023-10-26 11:16:18 +0500

    ConsultingComment added

---
+++ src/main/java/api/scolaro/uz/entity/ConsultingCommentEntity.java
@@ -0,0 +1,34 @@
+package api.scolaro.uz.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "person_role")
+@Getter
+@Setter
+public class ConsultingCommentEntity extends BaseEntity{
+
+    // consultingId
+    // studentId
+    // content (text)
+
+   // Student qaysidir consulting xizmatidan foydalangandna keyin shu consolting page da
+   // u haqida qandaydir text (comment) yozishi mumkun.
+   //  ConsultingComment ni faqat student role ga ega bo'lgan Profile create qiladi.
+
+    // quyidagi api lar qilinsin
+    //  1. POST  api/v1/consulting/comment/{consultingId} Create (consultingId, content) (studentId - jwtdan olinsin.)
+    //  Shu student va consulting o'rtasida AppApplicationEntity (ariza) bor bo'lishi kerak.
+       // va arizaning statusi FINISHED bo'lishi kerak.
+    // 2. GET api/v1/consulting/comment/{consultingId}  -  Get Consulting comment list - consulting id si bo'yicha
+    // shu consultingda yozilgan commentlarni yuboradi. visible = true bo'lganlarni
+    // Response (comment{id,createdDate,content, student{id,name,surname, photo{id,url}}}
+    // Bu open api permitAll bo'lsin.
+    // 3. GET api/v1/consulting/comment/filter - commentlarni filter qilish uchun. Only for admin
+    //  filter{studentId,consultingId,createdDate}.
+    //  Response (comment{id,createdDate,content, student{id,name,surname, photo{id,url}}, consulting{id,name, photo{id,url}}}
+
+}
