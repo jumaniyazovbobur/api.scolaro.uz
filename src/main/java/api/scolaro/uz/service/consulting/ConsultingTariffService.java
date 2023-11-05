9730d72809b63b73f12a7a981c983c6be7cc9ce5
MaxmudMaxmudov <maxmudmaxmudovp@gmail.com>
2023-11-05 23:24:06 +0500

    cunsulting tariff fixed

--- src/main/java/api/scolaro/uz/service/consulting/ConsultingTariffService.java
+++ src/main/java/api/scolaro/uz/service/consulting/ConsultingTariffService.java
@@ -61,7 +61,7 @@ public class ConsultingTariffService {
 
     public ApiResponse<String> update(ConsultingTariffUpdateDTO dto, String id) {
         ConsultingTariffEntity entity = get(id);
-        if (!entity.getConsultingId().equals(EntityDetails.getCurrentUserId())) {
+        if (entity.getConsultingId() == null || !entity.getConsultingId().equals(EntityDetails.getCurrentUserId())) {
             log.warn("Author is not incorrect or not found {}", entity.getConsultingId());
             throw new ItemNotFoundException("Author is not incorrect or not found");
         }
