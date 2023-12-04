d8ee8e0adfd185bfbdaa869ac99f80a842ebb7b9
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-12-04 12:15:48 +0500

    transaction entity

--- src/main/java/api/scolaro/uz/entity/transaction/TransactionsEntity.java
+++ src/main/java/api/scolaro/uz/entity/transaction/TransactionsEntity.java
@@ -1,5 +1,19 @@
-package api.scolaro.uz.entity;
+package api.scolaro.uz.entity.transaction;
 
+import api.scolaro.uz.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionsEntity extends BaseEntity {
     private String profileId; // Profile consulting
     private String profileType; // PROFILE,CONSULTING  to enum
@@ -8,7 +22,6 @@ public class TransactionsEntity extends BaseEntity {
     private String transactionType; // DEBIT, CREDIT to enum
     private String status;  // to enum
     private String paymentType; // PAYME,
-
 
 
     // TODO
