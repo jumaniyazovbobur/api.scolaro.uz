a597ab1628ca92e551cff4ff9b188c8f3d84340d
Mukhtarov Sarvarbek <mukhtarovsarvarbek@gmail.com>
2023-12-04 15:33:14 +0500

    Transaction

---
+++ src/main/java/api/scolaro/uz/controller/transaction/TransactionController.java
@@ -0,0 +1,19 @@
+package api.scolaro.uz.controller.transaction;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 'Mukhtarov Sarvarbek' on 04.12.2023
+ * @project api.scolaro.uz
+ * @contact @sarvargo
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/transaction")
+@Slf4j
+public class TransactionController {
+
+}
