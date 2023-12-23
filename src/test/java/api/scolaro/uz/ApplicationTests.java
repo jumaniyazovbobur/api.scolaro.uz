5934c33c712997d0f3f587373c11285977acc561
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2023-12-23 15:29:48 +0500

    ApplicationFilter changed

--- src/test/java/api/scolaro/uz/ApplicationTests.java
+++ src/test/java/api/scolaro/uz/ApplicationTests.java
@@ -1,12 +1,18 @@
 package api.scolaro.uz;
 
+import api.scolaro.uz.enums.AppLanguage;
+import api.scolaro.uz.service.ResourceMessageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-
+    @Autowired
+    private ResourceMessageService resourceMessageService;
     @Test
     void contextLoads() {
+        System.out.println(resourceMessageService.getMessage("not.found.attach", AppLanguage.ru));
+
     }
 }
