b39ba88b5998445be9b59bcca41a78acd1f695ba
Mukhtarov Sarvarbek <mukhtarovsarvarbek@gmail.com>
2024-01-18 11:05:03 +0500

    socket change

--- src/main/java/api/scolaro/uz/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/api/scolaro/uz/config/jwt/JwtAuthenticationFilter.java
@@ -13,6 +13,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -33,7 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private CustomUserDetailsService userDetailsService;
 
     @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    protected boolean shouldNotFilter(@NotNull HttpServletRequest request) throws ServletException {
         AntPathMatcher pathMatcher = new AntPathMatcher();
         return Arrays
                 .stream(SecurityConfiguration.AUTH_WHITELIST)
@@ -41,19 +42,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         final String header = request.getHeader("Authorization");
-        String tokenHeader = request.getParameter("token");
-        boolean tokenIsHeader = tokenHeader != null;
         if (header == null || !header.startsWith("Bearer ")) {
-            if (!tokenIsHeader) {
-                filterChain.doFilter(request, response);
-                return;
-            }
+            filterChain.doFilter(request, response);
+            return;
         }
 
         try {
-            final String token = tokenIsHeader ? tokenHeader : header.substring(7).trim();
+            final String token = header.substring(7).trim();
             JwtDTO jwtDTO = JwtUtil.decode(token);
             // load user depending on role
             UserDetails userDetails;
