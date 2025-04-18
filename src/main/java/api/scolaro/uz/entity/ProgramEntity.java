946f95785347a9b7b24aba28f0700c2a18acaecc
Bobur Jumaniyazov <bobur_jumaniyazov@epam.com>
2025-04-18 17:29:55 +0500

    ProgramEntity added som fields

--- src/main/java/api/scolaro/uz/entity/ProgramEntity.java
+++ src/main/java/api/scolaro/uz/entity/ProgramEntity.java
@@ -20,62 +20,22 @@ import java.util.List;
 @Table(name = "program")
 public class ProgramEntity extends BaseIdentityEntity {
 
-    @Column(name = "title_uz",length = 100,unique = true)
+    @Column(name = "title_uz", length = 100, unique = true)
     private String titleUz;
-
-    @Column(name = "title_ru",length = 100,unique = true)
+    @Column(name = "title_ru", length = 100, unique = true)
     private String titleRu;
-
-    @Column(name = "title_en",length = 100,unique = true)
+    @Column(name = "title_en", length = 100, unique = true)
     private String titleEn;
 
-    @Column(name = "description_uz", columnDefinition = "TEXT")
-    private String descriptionUz;
 
-    @Column(name = "description_ru", columnDefinition = "TEXT")
-    private String descriptionRu;
-
-    @Column(name = "description_en", columnDefinition = "TEXT")
-    private String descriptionEN;
-
-    @Column(name = "tuition_fees_description_uz")
-    private String tuitionFeesDescriptionUz;
-
-    @Column(name = "tuition_fees_description_ru")
-    private String tuitionFeesDescriptionRu;
-
-    @Column(name = "tuition_fees_description_en")
-    private String tuitionFeesDescriptionEn;
-
-    @Column(name = "scholarship_description_uz")
-    private String scholarshipDescriptionUz;
-
-    @Column(name = "scholarship_description_ru")
-    private String scholarshipDescriptionRu;
-
-    @Column(name = "scholarship_description_en")
-    private String scholarshipDescriptionEn;
-
-    @Column(name = "cost_of_living_description_uz")
-    private String costOfLivingDescriptionUz;
-
-    @Column(name = "cost_of_living_description_ru")
-    private String costOfLivingDescriptionRu;
-
-    @Column(name = "cost_of_living_description_en")
-    private String costOfLivingDescriptionEn;
+    @Column(name = "program_type")
+    @Enumerated(EnumType.STRING)
+    private ProgramType programType; // Тип
 
     @Column(name = "start_date")
-    private LocalDate startDate;
-
+    private LocalDate startDate; // Последний срок start
     @Column(name = "end_date")
-    private LocalDate endDate;
-
-    @Column(name = "price")
-    private Long price;
-
-    @Column(name = "symbol",length = 50)
-    private String symbol;
+    private LocalDate endDate; // Последний срок end
 
     @Column(name = "university_id")
     private Long universityId;
@@ -84,10 +44,45 @@ public class ProgramEntity extends BaseIdentityEntity {
     private UniversityEntity university; // TODO change to Institute
 
     @Column(name = "destination_id")
-    private Long destinationId;
+    private Long destinationId; // направление
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "destination_id", insertable = false, updatable = false)
     private DestinationEntity destination;
+    @Column(name = "study_duration")
+    private Integer studyDuration; // Продолжительность
+    @Column(name = "mainSubject")
+    private String mainSubject; // Основной предмет
+
+    // If scholar Ship -> грант
+    @Column(name = "price")
+    private Long price; // Стоимость гранта
+    @Column(name = "symbol", length = 50)
+    private String symbol;
+    @Column(name = "study_format")
+    @Enumerated(EnumType.STRING)
+    private StudyFormat studyFormat; // Формат обучения
+    @Column(name = "study_mode")
+    @Enumerated(EnumType.STRING)
+    private StudyMode studyMode; // Режим обучения
+
+    @Column(name = "tuition_fees_description_uz")
+    private String tuitionFeesDescriptionUz;  // Стоимость обучения uz
+    @Column(name = "tuition_fees_description_ru")
+    private String tuitionFeesDescriptionRu; // Стоимость обучения
+    @Column(name = "tuition_fees_description_en")
+    private String tuitionFeesDescriptionEn; // Стоимость обучения
+    @Column(name = "scholarship_description_uz")
+    private String scholarshipDescriptionUz; // Стипендия
+    @Column(name = "scholarship_description_ru")
+    private String scholarshipDescriptionRu; // Стипендия
+    @Column(name = "scholarship_description_en")
+    private String scholarshipDescriptionEn; // Стипендия
+    @Column(name = "cost_of_living_description_uz")
+    private String costOfLivingDescriptionUz; // Стоимость проживания
+    @Column(name = "cost_of_living_description_ru")
+    private String costOfLivingDescriptionRu;  // Стоимость проживания
+    @Column(name = "cost_of_living_description_en")
+    private String costOfLivingDescriptionEn;  // Стоимость проживания
 
     @Column(name = "attach_id")
     private String attachId;
@@ -95,19 +90,13 @@ public class ProgramEntity extends BaseIdentityEntity {
     @JoinColumn(name = "attach_id", insertable = false, updatable = false)
     private AttachEntity attach;
 
+    @Column(name = "description_uz", columnDefinition = "TEXT")
+    private String descriptionUz;
+    @Column(name = "description_ru", columnDefinition = "TEXT")
+    private String descriptionRu;
+    @Column(name = "description_en", columnDefinition = "TEXT")
+    private String descriptionEN;
+
     @Column(name = "published")
     private Boolean published = Boolean.FALSE;
-
-    @Column(name = "study_format")
-    @Enumerated(EnumType.STRING)
-    private StudyFormat studyFormat;
-
-    @Column(name = "study_mode")
-    @Enumerated(EnumType.STRING)
-    private StudyMode studyMode;
-
-    @Column(name = "program_type")
-    @Enumerated(EnumType.STRING)
-    private ProgramType programType;
-
 }
